21994448c210a0b5c09fdef2cdd499a124803096
DanTestehan <tdan89@yahoo.com>
2024-10-06 20:20:20 +0100

    chpt01-airbnb-experiences - new prj

---
+++ chpt02-airbnb-experiences/src/App.jsx
@@ -0,0 +1,29 @@
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
+function App() {
+
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                item={item}
+            />
+        )
+    })
+
+    // <Hero />
+    return (
+        <div>
+            <Navbar />
+            <Hero/>
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
+
+export default App
