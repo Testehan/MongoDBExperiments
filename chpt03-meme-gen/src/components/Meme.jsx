abe18cabfba8d85851bb2464f44a85c10adc94bf
DanTestehan <tdan89@yahoo.com>
2024-10-20 20:20:20 +0100

    chpt03-meme-gen - take memes pictures and data from an online API

--- chpt03-meme-gen/src/components/Meme.jsx
+++ chpt03-meme-gen/src/components/Meme.jsx
@@ -1,24 +1,7 @@
-import React from "react"
-import memeData from "../../memeData.js"
+import React, {useEffect} from "react"
+import memeData from "../../memeData.js"        // dummy local data..Next version of the app will use data obtained from an online API
 
 export default function Meme() {
-    /**
-     * Challenge: Update our state to save the meme-related
-     * data as an object called `meme`. It should have the
-     * following 3 properties:
-     * topText, bottomText, randomImage.
-     *
-     * The 2 text states can default to empty strings for now,
-     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
-     *
-     * Next, create a new state variable called `allMemeImages`
-     * which will default to `memesData`, which we imported above
-     *
-     * Lastly, update the `getMemeImage` function and the markup
-     * to reflect our newly reformed state object and array in the
-     * correct way.
-     */
-
 
     const [meme, setMeme] = React.useState({
         topText : "",
@@ -26,11 +9,26 @@ export default function Meme() {
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memeData)
+    const [allMemeImages, setAllMemeImages] = React.useState([])        // in the past we initialized this with "memeData", meaning the data from the file memeData.js
 
+
+    useEffect(() => {
+        fetchMemes();
+    }, []);
+
+
+    const fetchMemes = () => {
+        console.log("fetching memes");
+        const response =  fetch('https://api.imgflip.com/get_memes')
+            .then(response => response.json())
+            .then(jsonData => setAllMemeImages(jsonData.data.memes));       // because we want only the array of memes in the allMemeImages state, and we need to go inside the json with .data.memes
+
+    }
+
+    console.log(allMemeImages)
 
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
+        const memesArray = allMemeImages
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         const url = memesArray[randomNumber].url
         setMeme(previousMeme => ({
