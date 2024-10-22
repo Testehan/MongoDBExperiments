dde8a34371559daefa8b2cbc17cff59a5d92acde
DanTestehan <tdan89@yahoo.com>
2024-10-22 20:20:20 +0100

    chpt06-vidly - display movies in table with delete button

---
+++ chpt06-vidly/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+import Movies from "./components/movies.jsx";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <main role="main" className="container">
+            <h1>Welcome to Vidly app</h1>
+            <br />
+            <Movies/>
+        </main>
+    </>
+  )
+}
+
+export default App
---
+++ chpt06-vidly/src/components/movies.jsx
@@ -0,0 +1,61 @@
+import React, {Component} from "react";
+import {getMovies, deleteMovie} from "../services/fakeMovieService.js";
+
+class Movies extends Component{
+    state = {
+        movies: getMovies()
+    }
+
+    handleDelete = movie => {
+        console.log(movie);
+        deleteMovie(movie);
+        this.setState({movies: getMovies()})
+
+    }
+
+    render (){
+        const dbContainsMovies = this.state.movies.length>0;
+
+        if (dbContainsMovies) {
+            return (
+                <>
+                    <h1>Showing {this.state.movies.length} movies in the database.</h1>
+                    <table className="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Genre</th>
+                            <th scope="col">Stock</th>
+                            <th scope="col">Rate</th>
+                            <th scope="col"></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.movies.map((movie, index) =>
+                            <tr key={index + 1}>
+                                <th scope="row">{index + 1}</th>
+                                <td>{movie.title}</td>
+                                <td>{movie.genre.name}</td>
+                                <td>{movie.numberInStock}</td>
+                                <td>{movie.dailyRentalRate}</td>
+                                <td>
+                                    <button onClick={() => this.handleDelete(movie._id)}
+                                            className="btn btn-danger btn-sm">Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        )}
+                        </tbody>
+                    </table>
+                </>
+            )
+        } else {
+            return (
+                <h1>There are no movies in the database</h1>
+            )
+        }
+    }
+}
+
+export default Movies
