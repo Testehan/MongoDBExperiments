933cfaea95e72646994dd69fb684d782cdcaebc1
DanTestehan <tdan89@yahoo.com>
2024-11-10 20:20:20 +0100

    chpt06-vidly - login form refactored to use new Input component

--- chpt06-vidly/src/components/loginForm.jsx
+++ chpt06-vidly/src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Input from "./common/input.jsx";
 
 // Dan: refs are not recommended as the first solution....use them only in exceptional cases
 // const username = React.createRef();
@@ -26,33 +27,20 @@ class LoginForm extends Component {
             <div>
                 <h1>Login Form</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input autoFocus={true}
-                               value={this.state.account.username}  // this means that this input field will no longer have its own state.
-                                                                    // we are using props to set its value..so its value will always come
-                                                                    // from state.account.username
-                               onChange={this.handleChange}
-                               name="username"
-                               type="email" className="form-control" id="exampleInputEmail1"
-                               aria-describedby="emailHelp"
-                               placeholder="Enter email"/>
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone
-                            else.</small>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputPassword1">Password</label>
-                        <input type="password"
-                               value={this.state.account.password}
-                               onChange={this.handleChange}
-                               name="password"
-                               className="form-control" id="exampleInputPassword1"
-                               placeholder="Password"/>
-                    </div>
-                    <div className="form-group form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                    </div>
+                    <Input name="username"
+                           value={this.state.account.username}
+                        // this value means that this input field will no longer have its own state.
+                        // we are using props to set its value..so its value will always come
+                        // from state.account.username
+
+                           label="Username"
+                           onChange={this.handleChange}/>
+
+                    <Input name="password"
+                           value={this.state.account.password}
+                           label="Password"
+                           onChange={this.handleChange}/>
+
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
